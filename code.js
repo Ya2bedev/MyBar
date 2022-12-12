f8936febe0eaf959c29395f198d5d04714217abf
chhaya <brocyrip@gmail.com>
2022-12-12 23:44:35 +0700

    MyNavbr

---
+++ code.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    const date = new Date();
+    const hour = date.getHours();
+    let message = " ";
+    if (hour <= 12) {
+      message = "Good Morning";
+    }
+    if (hour > 12) {
+      message = "Good Afternoon";
+    }
+    if (hour > 17) {
+      message = "Good Evening";
+    }
+    if (hour > 21) {
+      message = "Good Night";
+    }
+    $("#GOOD")
+      .empty()
+      .append(" '' " + message + " '' " + " From CHHAYA");
+  });
